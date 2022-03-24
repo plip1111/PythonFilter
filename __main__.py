8b6ab78a3e8ef01a9be09726e0921760c511d291
Philip Pötzsch <Poetzsch.Philip@scheidt-bachmann-st.de>
2022-03-24 07:53:06 +0100

    added stuff

--- __main__.py
+++ __main__.py
@@ -1,43 +1,66 @@
+import imp
 import logging
 import threading
 import time
 import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
+from random import randint
 
+def apply_filter(xstart,xend,ystart,yend,rgb,img):
+    for i in range(xstart,xend):
+        for j in range(ystart,yend):
+            pass
+            apply_color(rgb, img, i, j)
 
-def thread_function(name, age):
-    logging.info("Thread %s: starting", name)
-    time.sleep(age)
-    logging.info("Thread %s: finishing", name)
+def apply_color(rgb, img, i, j):
+    r,g,b = img[i,j]
+    if rgb == 1:
+        img[i,j]=[add_100(r),g,b]
+        return
+    if rgb == 2:
+         img[i,j]=[r,add_100(g),b]
+         return
+    if rgb == 3:
+         img[i,j]=[r,g,add_100(b)]
+         return
 
+def add_100(num):
+    if num <=155:
+        return num+100
+    else:
+        return 255
+
+# hsv farbraum
 if __name__ == "__main__":
     img = cv2.imread("C:/ws_intern/src/PythonFilter/Sea-Turtle-PNG-Clipart.png")
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Fixes color read issue
 
-    av3 = cv2.blur(img,(3,3))
-    av5 = cv2.blur(img,(5,5))
-    # Plot the image. This code is excluded for the rest of the article.
-    plt.gcf().set_size_inches(25,25)
-    plt.subplot(131),plt.imshow(img),plt.title('Original')
-    plt.xticks([]), plt.yticks([])
-    plt.subplot(132),plt.imshow(av3),plt.title('Averaging - 3x3')
-    plt.xticks([]), plt.yticks([])
-    plt.subplot(133),plt.imshow(av5),plt.title('Averaging - 5x5')
-    plt.xticks([]), plt.yticks([])
-    plt.show()
+    rows, cols ,_= img.shape
+    size = 5
 
+    threads = []
+    print(f"size of picture : {rows} x {cols}")
+    for x in range(size):
+        for y in range(size):    
+            xstart = int(rows/size*x)
+            xend = int(rows/size*(x+1))
+            ystart = int(cols/size*y)
+            yend = int(cols/size*(y+1))
+            print(f"start = {xstart}, end = {xend}, ystart = {ystart}, yend = {yend}")
+            threads.append(threading.Thread(target=apply_filter, args=(xstart,xend,ystart,yend,randint(1,3),img)))
+
+    for thread in threads:
+        print(f"Thread {thread} started")
+        thread.start()
+
+    for thread in threads:
+        thread.join() 
+        print(f"Thread {thread} finished")
+        
     cv2.imwrite("C:/ws_intern/src/PythonFilter/output.png", img)
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
-    logging.info("Main    : before creating thread")
-    x = threading.Thread(target=thread_function, args=(1,2,))
-    y = threading.Thread(target=thread_function, args=(2,4,))
-    logging.info("Main    : before running thread")
-    x.start()
-    y.start()
-    logging.info("Main    : wait for the thread to finish")
-    y.join()
-    x.join()
+    
+    cv2.namedWindow("input", cv2.WINDOW_NORMAL)
+
+    cv2.imshow("input", img)
+    cv2.waitKey(0)
     logging.info("Main    : all done")
