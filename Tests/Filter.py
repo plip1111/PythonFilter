5745d962a39f606a4b03772517fe833fafca796b
Philip <poetzpu@gmail.com>
2022-04-28 08:41:41 +0200

    little cleanup

--- Tests/Filter.py
+++ Tests/Filter.py
@@ -6,53 +6,7 @@ class Filter():
     def __init__(self, doFilter, color):
         self.doFilter = doFilter
         self.color = color
-
-def apply_filter(xstart,xend,ystart,yend,rgb,img):
-    for i in range(xstart,xend):
-        for j in range(ystart,yend):
-            pass
-            apply_color(rgb, img, i, j)
-
-def apply_color(rgb, img, i, j):
-    r,g,b = img[i,j]
-    if rgb == 1:
-        img[i,j]=[add_100(r),g,b]
-        return
-    if rgb == 2:
-         img[i,j]=[r,add_100(g),b]
-         return
-    if rgb == 3:
-         img[i,j]=[r,g,add_100(b)]
-         return
-
-def add_100(num):
-    if num <=155:
-        return num+100
-    else:
-        return 255
-
-def filter_parallel(img,color):
-    rows, cols ,_= img.shape
-    size = 3
-    threads = []
-    initialcolor = color
-    for x in range(size):
-        for y in range(size):    
-            if(initialcolor == 4):
-                color = randint(1,3)
-            xstart = int(rows/size*x)
-            xend = int(rows/size*(x+1))
-            ystart = int(cols/size*y)
-            yend = int(cols/size*(y+1))
-            threads.append(threading.Thread(target=apply_filter, args=(xstart,xend,ystart,yend,color,img)))
-
-    for thread in threads:
-        thread.start()
-
-    for thread in threads:
-        thread.join() 
-    return img
-
+        
 def filter(img,color):
     rows, cols ,_= img.shape
     if(color == 1):
@@ -81,3 +35,50 @@ def filter(img,color):
     img = cv2.bitwise_and(img,filtercolor)
 
     return img
+
+def filter_parallel(img,color):
+    rows, cols ,_= img.shape
+    size = 3
+    threads = []
+    initialcolor = color
+    for x in range(size):
+        for y in range(size):    
+            if(initialcolor == 4):
+                color = randint(1,3)
+            xstart = int(rows/size*x)
+            xend = int(rows/size*(x+1))
+            ystart = int(cols/size*y)
+            yend = int(cols/size*(y+1))
+            threads.append(threading.Thread(target=apply_filter, args=(xstart,xend,ystart,yend,color,img)))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join() 
+    return img
+
+def apply_filter(xstart,xend,ystart,yend,rgb,img):
+    for i in range(xstart,xend):
+        for j in range(ystart,yend):
+            pass
+            apply_color(rgb, img, i, j)
+
+def apply_color(rgb, img, i, j):
+    r,g,b = img[i,j]
+    if rgb == 1:
+        img[i,j]=[add_100(r),g,b]
+        return
+    if rgb == 2:
+         img[i,j]=[r,add_100(g),b]
+         return
+    if rgb == 3:
+         img[i,j]=[r,g,add_100(b)]
+         return
+
+def add_100(num):
+    if num <=155:
+        return num+100
+    else:
+        return 255
+
